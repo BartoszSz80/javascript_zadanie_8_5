4535429a6ed380e9775aa360ed393637bfbd3c8f
BartoszSz80 <bartek@bartweb.eu>
2017-03-28 16:13:39 +0200

    files added

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+// plik scripts.js
+	var a = 5;   
+	var h = 6;    
+	var triangleArea = (a*h/2);
+	console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
