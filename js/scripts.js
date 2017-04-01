a15883f9e44a090eb12b993a8a64e19c92096bc8
BartoszSz80 <bartek@bartweb.eu>
2017-04-01 13:41:51 +0200

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
 // plik scripts.js
-	var a = 5;   
-	var h = 6;    
-	var triangleArea = (a*h/2);
-	console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+	var a = 5;
+	var b = 6;
+		var value = (a * a) + (2 * a * b) - (b * b);
+		console.log('Wynik: ' + value);
+
+	if (value => 0) {
+		console.log('wynik dodatni');
+	}
+	else if (value < 0) {
+		console.log('wynik ujemny');
+	}
+
+	if (value == 0) {
+		console.log('wynik równy 0');
+	}
+	else if (value != 0) {
+		console.log('wynik nie jest równy 0');
+	}
